9665bd82f5a67109ee0fadda8054cdcb618b78fd
ThéophileLPE <92462938+Tyosa720@users.noreply.github.com>
2023-09-29 11:25:06 +0200

    part 1

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Auth from "./components/auth/auth";
+
+function App() {
+  return <div>test</div>;
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Auth from "./components/auth/auth";
+import Chat from "./Pages/Chat/chat";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />}></Route>
+        <Route path="/login" element={<Auth></Auth>} />
+        <Route path="/chat" element={<Chat></Chat>} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
